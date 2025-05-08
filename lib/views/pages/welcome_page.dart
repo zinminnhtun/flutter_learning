e1a608954b16c98f408a25db5087caa2a64f8432
zinminnhtun <zinmintoun@gmail.com>
2025-05-08 08:52:22 +0700

    FittedBox

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter_app/views/widget_tree.dart';
 
 import 'package:lottie/lottie.dart';
 
-
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
@@ -14,18 +13,49 @@ class WelcomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,      
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Lottie.asset('assets/lotties/lottie1.json'),
-            Text("Flutter App",style: TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold,letterSpacing: 50.0),),
-            FilledButton(onPressed: (){Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return WidgetTree();
-                  },
+            FittedBox(
+              child: Text(
+                "Flutter App",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 50.0,
                 ),
-              );}, child: Text('Login')),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            FilledButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WidgetTree();
+                    },
+                  ),
+                );
+              },
+              style: FilledButton.styleFrom(
+                minimumSize: Size(double.infinity, 40.0),
+              ),
+              child: Text('Login'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WidgetTree();
+                    },
+                  ),
+                );
+              },
+              child: Text('Login'),
+            ),
           ],
         ),
       ),
